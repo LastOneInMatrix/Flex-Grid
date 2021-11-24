3533ce757581910675ff38a3ab52bc87310e3636
Barsov Ashot <matrixngnl@gmail.com>
2021-11-24 16:20:31 +0300

    --first grid lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import './App.css';
+import {Grid1} from "./GridTutorials/FisrtStaps/1";
 
 function App() {
   return (
     <div className="App">
-        <div className={"flex-container"}>
-            <div className={'flex-item absolute1'}>static</div>
-            <div className={'flex-item absolute2'}>relative</div>
-            {/*<div className={'flex-item absolute'}>absolute</div>*/}
-            {/*<div className={'flex-item fixed'}>fixed</div>*/}
-            {/*<div className={'flex-item sticky'}>sticky</div>*/}
-        </div>
+       < Grid1/>
     </div>
   );
 }
---
+++ src/GridTutorials/FisrtStaps/1.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './1.css';
+
+export const Grid1 = () => {
+    return <div className='mainGridContainer'>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cum, deleniti eos inventore magnam mollitia nam natus non perferendis, quia rem totam.
+        </div>
+        <div className='gridItem'>
+           Lorem ipsum dolor.
+        </div>
+        <div className='gridItem'>
+           Lorem ipsum dolor sit amet.
+        </div>
+        <div className='gridItem'>
+           Lorem ipsum dolor sit amet, consectetur adipisicing.
+        </div>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, animi asperiores culpa ea eligendi est facilis hic id illo nobis praesentium quaerat tempora tenetur unde velit vitae voluptatibus? Natus, non.
+        </div>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit!
+        </div>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit!
+        </div>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, quidem.
+        </div>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, aperiam, illo.
+        </div>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit!
+        </div>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit!
+        </div>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, quidem.
+        </div>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, aperiam, illo.
+        </div>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit!
+        </div>
+        <div className='gridItem'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit!
+        </div>
+
+
+    </div>
+}
